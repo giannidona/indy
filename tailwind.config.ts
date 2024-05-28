6b2f258e7e35c7617fad5994db75ad730eed3e3d
Gianluca Donato <gianlucadonato2005@gmail.com>
2024-05-28 11:26:13 -0300

    🗑️ clean project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        eBlack: "#09090B",
+        eWhite: "#E6E6E6",
+        eGray: "#AfADA7",
       },
     },
   },
