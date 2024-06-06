02a56af7b6bb78d9a8271ce667e128805cd8dc44
Gianluca Donato <gianlucadonato2005@gmail.com>
2024-06-06 19:51:44 -0300

    feat: 🎨 tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,13 +5,14 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         eBlack: "#09090B",
         eWhite: "#E6E6E6",
-        eGray: "#AfADA7",
+        eGray: "#989792",
       },
     },
   },
