b85127a08422d6cb07a6e351d8e679c41200a6f9
Gianluca Donato <gianlucadonato2005@gmail.com>
2024-06-07 21:58:06 -0300

    feat: 📋 add copy to clipboard

--- src/app/docs/_components/ShowCode.tsx
+++ src/app/docs/_components/ShowCode.tsx
@@ -1,16 +1,27 @@
+"use client";
+
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { FaRegCopy } from "react-icons/fa";
 
 interface Props {
   code: string;
   fileName?: string;
 }
 
+const onCopyToClipboard = (code: string) => {
+  const copyCode = navigator.clipboard.writeText(code);
+  return copyCode;
+};
+
 export const ShowCode = ({ code, fileName }: Props) => {
   return (
     <>
-      <div className="py-2 px-4 border border-eWhite/50 rounded-t-xl h-auto">
+      <div className="py-2 px-4 border border-eWhite/50 rounded-t-xl h-auto flex justify-between items-center">
         {fileName}
+        <p onClick={() => onCopyToClipboard(code)} className="cursor-pointer">
+          <FaRegCopy />
+        </p>
       </div>
       <div className="mb-12 py-2 px-4 border border-eWhite/50 rounded-b-xl h-auto">
         <SyntaxHighlighter
