7da4f708b112861d6fbb80be896d1b9a887cb41b
Gianluca Donato <gianlucadonato2005@gmail.com>
2024-06-08 20:25:57 -0300

    fix: 🔨 new badge style

--- src/_components/badges/IconBadge.tsx
+++ src/_components/badges/IconBadge.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export const IconBadge = () => {
   return (
-    <span className="bg-black text-white border border-white text-sm flex items-center w-fit px-3 rounded-md transition ease-in-out duration-300 hover:-translate-y-1 hover:scale-110">
+    <span className="bg-black text-white border border-white text-sm flex items-center w-fit px-3 py-1 rounded-md">
       <Image
         className="mr-2"
-        src="/favicon.png"
+        src="/icon.png"
         alt="logo"
         width={15}
         height={15}
