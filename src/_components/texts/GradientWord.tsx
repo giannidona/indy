f77d74ace4a725e7b652bd6ab26592f4a221d167
Gianluca Donato <gianlucadonato2005@gmail.com>
2024-06-08 20:26:56 -0300

    feat: 🎨 gradient text

---
+++ src/_components/texts/GradientWord.tsx
@@ -0,0 +1,12 @@
+export const GradientWord = () => {
+  return (
+    <div>
+      <p className="font-bold text-5xl">
+        <span className="bg-gradient-to-r from-cyan-500 to-blue-500 text-transparent bg-clip-text">
+          Gradient
+        </span>{" "}
+        word
+      </p>
+    </div>
+  );
+};
